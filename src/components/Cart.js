a52775370266af09a82f7a1c3ae32b73934136d1
IdoShmida <shmidamail@gmail.com>
2021-05-03 16:47:10 +0300

    added of item to cart horrable css

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -11,7 +11,7 @@ return (
     <p>this is the cart:</p> <br></br>
     {cartItems.map((obj, index) => 
         <div key={index} className="cart-item">
-
+                <img alt="" height="42" width="42" src={obj.image}></img>
             <div > item no.{obj.id} : {obj.title} price: {obj.price}$</div>
             <hr></hr><br></br>
 
